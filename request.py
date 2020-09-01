341d91756da50b3d5d65320a30aaa43ac3113f79
jonilsenGithub <62014422+jonilsenGithub@users.noreply.github.com>
2020-09-01 13:26:51 +0200

    Update request.py

--- request.py
+++ request.py
@@ -5,28 +5,35 @@ import random
 import json
 from pytictoc import TicToc
 
-#API
+#defining URL to the endpoint where the applications are deployed
 URL = "http://ec2-3-235-65-99.compute-1.amazonaws.com/"
 
-#parameters to be sent in request
+#importing the same dataset as in the Cassandra database
 df = pd.read_csv('kd.csv')
+#cleaning sample for unnecessary columns and correcting formatting
 df = df.drop(['sessionindex','rep'],axis=1)
 df['subject'] = df['subject'].apply(lambda x: int(x[2:]))
 
+#selecting random sample from dataset
 sample = np.array(df.iloc[random.randint(0,20400)])
 
+#the tolist() method converts the sample series to a list, which is is then transformed to JSON-format
 data = json.dumps(sample.tolist())
 
+#defining parameters sent to the URL endpoint as a dictionary
 PARAMS = ({'data':data})
 
-#measuring processing time
+#instanciating the timing module
 t = TicToc()
 
+#starting timer
 t.tic()
 
-#request and response
+#sending request and storing response in the variable resp
 resp = requests.get(url = URL, params = PARAMS)
 
+#stopping timer
 t.toc()
 
+#printing response in JSON-format because thats the format it is received in
 print(resp.json())
